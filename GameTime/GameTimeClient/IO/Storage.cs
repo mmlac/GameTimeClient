c8dde540217e3960c9c422b5638e4a4b73e5d8c1
Markus Lachinger <misc@mmlac.com>
2016-10-02 17:52:06 -0700

    Removed ping table
    Related Work Items: #1

--- GameTime/GameTimeClient/IO/Storage.cs
+++ GameTime/GameTimeClient/IO/Storage.cs
@@ -17,31 +17,14 @@ namespace GameTimeClient.Tracking.IO
             String.Format("Data Source={0};Version=3;Pooling=True;" +
                 "Max Pool Size=10;", DATABASE_FILENAME);
 
-
-        private const String CREATE_PING_TABLE =
-                    "CREATE TABLE IF NOT EXISTS ping (" +
-                    "id INTEGER PRIMARY KEY, " +
-                    "time DEFAULT CURRENT_TIMESTAMP" +
-                    ")";
-
         private const String CREATE_PROC_3D_TABLE =
             "CREATE TABLE IF NOT EXISTS proc3d (" +
             "id INTEGER PRIMARY KEY, " +
             "time DEFAULT CURRENT_TIMESTAMP, " +
             "programs TEXT)";
 
-
-        private const String INSERT_PING_CMD =
-            "INSERT INTO ping DEFAULT VALUES";
-
         private const String INSERT_PROC_3D_TEMPLATE =
             "INSERT INTO proc3d (programs) VALUES ('{0}');";
-
-        private const String SELECT_ALL_PINGS =
-            "SELECT time FROM ping";
-
-        private const String DELETE_PINGS_UNTIL =
-            "DELETE FROM ping WHERE time <= '{0}'";
 
         private const String SELECT_ALL_3D_PROCS =
             "SELECT time, programs FROM proc3D";
@@ -61,16 +44,10 @@ namespace GameTimeClient.Tracking.IO
             using (var sqlConn =
                 new SQLiteConnection(DATABASE_CONNECTION_STRING))
             {
-
-
-                SQLiteCommand create_ping =
-                    new SQLiteCommand(CREATE_PING_TABLE, sqlConn);
-
                 SQLiteCommand create_proc3d =
                     new SQLiteCommand(CREATE_PROC_3D_TABLE, sqlConn);
 
                 sqlConn.Open();
-                create_ping.ExecuteNonQuery();
                 create_proc3d.ExecuteNonQuery();
             };
         }
@@ -92,66 +69,15 @@ namespace GameTimeClient.Tracking.IO
                 String procString = String.Join(",", procNames);
                 String procQuery = 
                     String.Format(INSERT_PROC_3D_TEMPLATE, procString);
-                
-                var pingCmd = 
-                    new SQLiteCommand(INSERT_PING_CMD, sqlConn);
                 var procCmd = new SQLiteCommand(procQuery, sqlConn);
 
                 sqlConn.Open();
-                using (var transaction = sqlConn.BeginTransaction())
-                {
-                    pingCmd.ExecuteNonQuery();
-                    procCmd.ExecuteNonQuery();
-                    transaction.Commit();
-                }
+                procCmd.ExecuteNonQuery();
+                
 
             }
         }
 
-        public List<DateTime> getPings()
-        {
-            var pingTimes = new List<DateTime>();
-            using (var sqlConn = 
-                new SQLiteConnection(DATABASE_CONNECTION_STRING))
-            {
-                var getPingsCommand = 
-                    new SQLiteCommand(SELECT_ALL_PINGS, sqlConn);
-
-                sqlConn.Open();
-                SQLiteDataReader r = getPingsCommand.ExecuteReader();
-
-                while(r.Read())
-                {
-                    try
-                    {
-                        pingTimes.Add(
-                            DateTime.SpecifyKind(
-                                DateTime.Parse(Convert.ToString(r["time"])),
-                                DateTimeKind.Utc
-                                ));
-                    } catch (Exception e)
-                    {
-                        Console.WriteLine(
-                            "Error reading the following DateTime: {0} - {1}",
-                            r["time"], e.Message);
-                    }
-                }
-
-                return pingTimes;
-            }
-        }
-
-        /// <summary>
-        ///     Delete all the (processed) ping entries up to (and including)
-        ///     the given DateTime
-        /// </summary>
-        /// <param name="to">inclusive end time of delete</param>
-        public void deletePingsUpTo(DateTime to)
-        {
-            SQLiteCommand delCmd = new SQLiteCommand(String.Format(
-                DELETE_PINGS_UNTIL,
-                to.ToUniversalTime().ToString("yyyy'-'MM'-'dd HH':'mm':'ss")));
-        }
 
 
         public List<Tuple<DateTime, String>> get3DProcesses()
